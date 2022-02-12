65efe9d5176824f93537520fd7a0075912ec3a81
aligneddata <aligneddata@gmail..com>
2022-02-11 21:50:38 -0800

    verified spark to db connection from spark-submit by python

---
+++ README.md
@@ -0,0 +1,13 @@
+# Purpose
+This project aims to setup a docker development quickly with most dev servers needed.
+
+# Run
+docker-compose -f devnet.yml up --scale spark-worker=2
+
+# Data
+database in mariadb:
+https://dev.mysql.com/doc/employee/en/employees-installation.html
+
+database in postres:
+database: devdb
+table: t (i int)
--- start_dns_proxy.sh
+++ start_dns_proxy.sh
@@ -3,9 +3,9 @@
 #   docker network create -d bridge devnet
 #   provision static ip on host
 #   provision local dnsmasq.conf right (use 127.0.0.1 first, then other follows).
-# each run:
-#   docker-compose -f dev.yml up
-#   (run this script)
+
+# setup:
+# @reboot ~/git/base-docker-env/start_dns_proxy.sh > ~/logs/start_dns_proxy.log 2>&1
 
 docker run --hostname dns.mageddo --restart=unless-stopped -p 5380:5380 \
 -v /var/run/docker.sock:/var/run/docker.sock \
