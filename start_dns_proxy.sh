f6c19a996c59cc3b28d53bee73f4c5c1a1da2447
aligneddata <aligneddata@gmail..com>
2022-10-04 07:36:24 -0700

    more updates

--- start_dns_proxy.sh
+++ start_dns_proxy.sh
@@ -11,5 +11,6 @@ docker run --hostname dns.mageddo --restart=unless-stopped -p 5380:5380 \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /etc/resolv.conf:/etc/resolv.conf \
 --network=devnet \
+--name dns-proxy-server \
 defreitas/dns-proxy-server      
 
