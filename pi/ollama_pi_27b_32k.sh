7d30ab40c3bff3d5b29dec39a04ee8ac59d18767
adironca <adironca@gmail.com>
2026-08-29 22:57:51 -0700

    archive save

--- pi/ollama_pi_27b_32k.sh
+++ pi/ollama_pi_27b_32k.sh
@@ -1,8 +1,8 @@
 docker run -it --rm \
   --net=host \
   -v "$(pwd):/workspace" \
-  -v "$HOME/git/base-docker-env/pi/models.json:/root/.pi/agent/models.json" \
-  -v "$HOME/git/base-docker-env/pi/settings.json:/root/.pi/agent/settings.json" \
+  -v "$HOME/git/base-docker-env/pi/models.json.27b.32k:/root/.pi/agent/models.json" \
+  -v "$HOME/git/base-docker-env/pi/settings.json.27b.32k:/root/.pi/agent/settings.json" \
   pi-agent \
   --provider ollama
 
